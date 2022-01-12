8ed9ce45ec7a69ab62a1c7c81c087d5503dea032
Daniel Lovett <dlovett@innovateteam.com>
2022-01-11 18:57:10 -0600

    adding environment variable code

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
+import os
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///wine.sqlite"
@@ -8,14 +10,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+my_key = os.environ.get('MY_SECRET_KEY')
+
 engine = db.create_engine(app.config['SQLALCHEMY_DATABASE_URI'], {})
 
 @app.route("/")
 def home():
-    # engine.execute('SELECT * FROM wines')
+    html = f'<div>{my_key}</div>'
     with engine.connect() as conn:
         df = pd.read_sql('wines', conn)
-        html = df.to_html()
+        html += df.to_html()
     return html
 
 if __name__ == "__main__":
